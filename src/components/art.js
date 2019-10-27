4550871efcb3988ca9b272f2592ab2dbd3e5f269
jacobcalv <jacobcalvino@outlook.com>
2019-10-27 14:12:06 -0500

    fixed bug with reducer

--- src/components/art.js
+++ src/components/art.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux';
+import {fetchArt} from '../actions/artApi'
 
-const Art = () => {
+const Art = (props) => {
+    console.log(props)
+    useEffect(() => {
+        props.fetchArt();
+    },[])
+    console.log(props)
+
     return (
         <div>
-            
+            {props.image && props.image.map((image,index) => (
+                <img key={index} src={image.baseimageurl} alt={image.copyright}/>
+            ))}
         </div>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        image: state.art
+    }
+}
+
+const mapDispatchToProps = {
+    fetchArt
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Art);
