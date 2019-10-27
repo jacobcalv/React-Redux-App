861578fd061fa4444114def1b8b927fb87c2acca
jacobcalv <jacobcalvino@outlook.com>
2019-10-27 15:26:30 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Art from './components/Art'
-
+import Display from './components/Display'
 function App() {
+
   return (
+    <>
+      <Display/>
       <Art/>
+    </>
   );
 }
 
--- src/components/art.js
+++ src/components/art.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux';
 import {fetchArt} from '../actions/artApi'
+import styled from 'styled-components'
 
 const Art = (props) => {
     console.log(props)
@@ -9,12 +10,40 @@ const Art = (props) => {
     },[])
     console.log(props)
 
+    const IMG = styled.img`
+        width: 16rem;
+    `
+    const Header = styled.h1`
+        text-align: center;
+    `
+    const Container = styled.div`
+        padding: 1rem;
+        width: 16rem;
+        margin-right: 2rem;
+        background-color: white ;
+        border-left: 1rem solid #8D6142;
+        border-right: 1rem solid #8D6142;
+        border-bottom: 1rem solid grey;
+        border-top: 1rem solid grey;
+        margin-bottom: 2rem;
+        align-self: center;
+    `
+    const Images = styled.div`
+        display: flex;
+        width: 80%;
+        margin-left: 12%;
+        margin-right: 10%;
+        flex-wrap: wrap;
+    `
     return (
-        <div>
+        <Images>
             {props.image && props.image.map((image,index) => (
-                <img key={index} src={image.baseimageurl} alt={image.copyright}/>
+                <Container>
+                <IMG key={index} src={image.baseimageurl} alt={image.copyright}/>
+                <Header>ID {image.fileid}</Header>
+                </Container>
             ))}
-        </div>
+        </Images>
     )
 }
 
